552bf7c3e012a58ef810a4f29b18837e212b9112
Damian <damianfarinaccio@gmail.com>
2022-10-19 18:00:17 +1100

    added playerdata

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,65 @@
+/* 
+This function accepts 2 props 
+
+1. endpoint: this is the endpoint you want to use for example player,teams and games are
+types of endpoints that are available for this API
+
+2. id: for when you want a specific entry, say you want a specific player, then you will
+need to specify their id. If you omit id prop, then it will fetch all results from the endpoint.
+
+*/
 import React, { useState, useEffect } from 'react';
 
-export function Api() {
+function determineReturn(nbaData, endpoint, id) {
+	let outputHtml = <p>Error: No Output</p>
+	if (!id == undefined) {
+		switch (endpoint) {
+			case 'players':
+				// console.log("players endpoint fetched")
+				outputHtml = (
+					<div>
+						<ul>{
+							nbaData.map(d => (<li key={d.id}>{d.first_name}</li>))
+						}
+						</ul>
+					</div>
+				)
+				break;
+
+			case 'teams':
+				outputHtml = <p>Teams endpoint selected</p>
+				break;
+
+			default:
+				outputHtml = <p>ERROR: could not determine endpoint</p>
+
+		}
+	}
+	else {
+		switch (endpoint) {
+			case 'players':
+				outputHtml = <li key={id}>{`${nbaData.first_name} ${nbaData.last_name}`}</li>
+				break;
+
+			default:
+				break;
+		}
+	}
+	// console.log(outputHtml)
+	return outputHtml
+}
+
+export default function Api(props) {
+	let fetchUrl = ""
+	const [nbaData, setNbaData] = useState([])
+
+	if (!props.id) {
+		fetchUrl = `https://free-nba.p.rapidapi.com/${props.endpoint}?page=0`
+	}
+	else {
+		fetchUrl = `https://free-nba.p.rapidapi.com/${props.endpoint}/${props.id}`
+	}
+
 	const options = {
 		method: 'GET',
 		headers: {
@@ -8,10 +67,17 @@ export function Api() {
 			'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
 		}
 	};
-	
-	fetch('https://free-nba.p.rapidapi.com/teams/1', options)
-		.then(response => response.json())
-		.then(response => console.log(response))
-		.catch(err => console.error(err));
-}
 
+	useEffect(() => {
+		fetch(fetchUrl, options)
+			.then(response => response.json())
+			.then((response) => {
+				setNbaData(response.data)
+				// console.log(`data = ${JSON.stringify(response.data)}`)
+				// console.log(`nbaData = ${JSON.stringify(nbaData)}`)
+			})
+			.catch(err => console.error(err));
+	}, [])
+
+	return determineReturn(nbaData, props.endpoint, props.id)
+}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,75 @@
-
+import { useState, useEffect } from 'react'
 import './App.css';
-import {Api} from './Api.js';
+import Api from './Api.js';
+// import PlayersTable from './PlayersTable';
+import React from 'react';
+import playerData from './Playerdata.json'
 
 function App() {
+  const [ps, setPS] = useState([])
+  const playerIDs = playerData.players
+  const [player1ID, setP1ID] = useState(0)
+
+  const [player1, setPlayer1] = useState({})
+  const [player2, setPlayer2] = useState(playerIDs[1])
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ccdcf26f0amsh0a05285afe6fe1ap1bc969jsn2f978ec224f0',
+      'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+    }
+  }
+
+  // useEffect(() => {
+  //   setPS(playerIDs.map( p => {
+  //     fetch(`https://free-nba.p.rapidapi.com/players/${p.id}`,options)
+  //     .then(r => r.json())
+  //     .then(d => p = d )
+  //     .catch(err => console.log(err))
+
+  //   }
+  //   ))
+  // },[])
+
+  useEffect(() => {
+
+      fetch(`https://free-nba.p.rapidapi.com/players/${player1ID}`, options)
+        .then(response => response.json())
+        .then((data) => {
+          setPlayer1(data)
+          console.log(data)
+        })
+        .catch(err => console.error(err));
+  
+  }, [player1ID])
+
+  // const playerIDs = playerData.players
+  // let players = []
+  // const [nbaData, setNbaData] = useState([])
+
+
+
+  // const fetchPlayers = (players) => {
+  //   for (const player of players) {
+  //     fetch(`https://free-nba.p.rapidapi.com/players/${player.id}`, options)
+  //       .then(response => response.json())
+  //       .then(data => players.push(data))
+  //       .catch(err => console.error(err));
+  //   }
+  // }
+
+  // useEffect(fetchPlayers(playerIDs), [])
+
   return (
     <div>
       <h1>NBA Rosters</h1>
-      <Api/>
+      <h2>You have selected:</h2>
+      <p>{player1.first_name}</p>
+        {playerIDs.map((player) => {
+          return <button key={player.id} onClick={((e) => {setP1ID(player.id)})}>{player.name}</button>
+          console.log(player)
+        })}
     </div>
   );
 }
