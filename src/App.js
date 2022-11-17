5d8b878830e50c159c4698282d0fe3773569dc33
Damian <damianfarinaccio@gmail.com>
2022-11-18 02:20:12 +1100

    patch

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'ccdcf26f0amsh0a05285afe6fe1ap1bc969jsn2f978ec224f0',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
     }
   }
